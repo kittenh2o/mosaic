2b337f44341a04a6a96e739405ee915af0040bd9
huai <yanghuai2012@outlook.com>
2019-04-07 10:21:21 +0800

    add image processor

--- main/core/process_pic.py
+++ main/core/process_pic.py
@@ -12,4 +12,28 @@ class Image:
     def height(self) -> int:
         return self.data.shape[0]
 
-    
+    def size(self) -> int:
+        return self.width()*self.height()
+
+class ImageProcessor:
+    def eval_rgb(self, image:Image) -> dict:
+        r_sum = g_sum = b_sum = 0
+        for col in image.data:
+            for pixel in col:
+                r_sum += pixel[0]
+                g_sum += pixel[1]
+                b_sum += pixel[2]
+        
+        r_avg, g_avg, b_avg = r_sum/image.size(), g_sum/image.size(), b_sum/image.size()
+
+        return {
+            "r": r_avg,
+            "g": g_avg,
+            "b": b_avg
+        }
+
+    def diff(self, image1:Image, image2:Image) -> int:
+        rgb1 = self.eval_rgb(image1)
+        rgb2 = self.eval_rgb(image2)
+
+        return ((rgb1['r']-rgb2['r'])**2 + (rgb1['g']-rgb2['g'])**2 + (rgb1['b']-rgb2['b'])**2) 
--- main/test/test_read_image.py
+++ main/test/test_read_image.py
@@ -1,9 +1,9 @@
 import sys
 import unittest
-from os.path import dirname
+from os.path import dirname, join
 
 sys.path.insert(0, dirname(dirname(__file__)))
-from core.process_pic import Image
+from core.process_pic import Image, ImageProcessor
 
 class TestReadImage(unittest.TestCase):
     
@@ -22,8 +22,28 @@ class TestReadImage(unittest.TestCase):
             image = Image(uri)
             self.assertEqual(size[0], image.width())
             self.assertEqual(size[1], image.height())
+            self.assertEqual(size[0]*size[1], image.size())
 
+
+class TestImageProcessor(unittest.TestCase):
+
+    def setUp(self):
+        red_uri = join(dirname(__file__), "image_resources", "red.png")
+        green_uri = join(dirname(__file__), "image_resources", "green.png")
+        blue_uri = join(dirname(__file__), "image_resources", "blue.png")
+
+        self.red_image, self.green_image, self.blue_image = Image(red_uri), Image(green_uri), Image(blue_uri)
+        
+
+    def test_eval_image(self):
+        self.assertEqual(ImageProcessor().eval_rgb(self.red_image)["r"], 255)
+        self.assertEqual(ImageProcessor().eval_rgb(self.green_image)["g"], 255)
+        self.assertEqual(ImageProcessor().eval_rgb(self.blue_image)["b"], 255)
+
+    def test_image_diff(self):
+        self.assertEqual(ImageProcessor().diff(self.red_image, self.blue_image), 2*255**2)
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestReadImage)
-    unittest.TextTestRunner().run(suite)
+    suites = [unittest.TestLoader().loadTestsFromTestCase(TestClass) for TestClass in [TestImageProcessor, TestReadImage]]
+    for suite in suites:
+        unittest.TextTestRunner(verbosity=2).run(suite)
